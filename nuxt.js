026066cc91182b20f49cb676723b1ceac7b3a417
Daniel <dancardosobr@gmail.com>
2020-07-21 08:22:55 +0100

    chore: build

---
+++ nuxt.js
@@ -0,0 +1,34 @@
+const { Nuxt, Builder } = require('nuxt')
+
+const https = require('https')
+const fs = require('fs')
+const isProd = (process.env.NODE_ENV === 'production')
+const port = process.env.PORT || 3002
+
+// We instantiate nuxt.js with the options
+const config = require('./nuxt.config.js')
+config.dev = !isProd
+const nuxt = new Nuxt(config)
+
+// Render every route with Nuxt.js
+
+// Build only in dev mode with hot-reloading
+if (config.dev) {
+  new Builder(nuxt).build()
+    .then(listen)
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+} else {
+  listen()
+}
+
+function listen () {
+  const options = {
+    key: fs.readFileSync('ssl.key'),
+    cert: fs.readFileSync('ssl.crt')
+  }
+  https.createServer(options, nuxt.render).listen(port)
+  console.log('Server listening on `localhost:' + port + '`.')
+}
